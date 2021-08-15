930bc64c5237d96c889cf7ce3882ada6ef599f84
harshit.jadon <harshit.jadon@MD-CJDKE06555KH.magicsw.com>
2021-08-15 16:19:05 +0530

    {Harshit} CREATE Footer of Bookstore HomePage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from 'C:/Users/harshit.jadon.MD-CJDKE06555KH/Desktop/Bookstore App/bookstore-app/src/components/header/header.jsx';
+import Footer from 'C:/Users/harshit.jadon.MD-CJDKE06555KH/Desktop/Bookstore App/bookstore-app/src/components/footer/footer.jsx';
 
 function App() {
   return (
     <>
     <Header />
+    <Footer />
      </>
   );
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'C:/Users/harshit.jadon.MD-CJDKE06555KH/Desktop/Bookstore App/bookstore-app/src/components/footer/footer.scss';
+
+export default function footer() {
+    return (
+       
+            <footer className='footer-content'>
+                <div className='footer-div'>
+                    <p> Copyright &copy; 2020. Bookstore Private Limited. All Rights Reserved</p>
+                </div>
+            </footer>
+    
+    )
+}
