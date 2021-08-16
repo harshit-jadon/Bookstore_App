c1c8572444cefb04341ce1206969bb91ffa8cde8
anuj26298 <kumar26298anuj@gmail.com>
2021-08-16 10:11:00 +0530

    {Anuj} CREATE item count number on cart logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,6 +5,15 @@ import cartLogo from '../../assests/images/supermarket.svg';
 import searchLogo from '../../assests/images/ic_zoom_out_24px.svg';
 
 export default function Header() {
+
+    const cartSize=()=>{
+        const cart=JSON.parse(localStorage.getItem("cart"))
+        if(cart) {
+            console.log(cart.length)
+            return cart.length
+        }
+    }
+
     return (
         <div>
            <header class='header-content'>
@@ -18,7 +27,7 @@ export default function Header() {
                 </div>
                 <div class='cart-logo'>
                     <span>Cart</span>
-                    <img src={cartLogo} alt='cartLogo'/>
+                    <img src={cartLogo} alt='cartLogo'/><span className="cart-count">{cartSize()}</span>
                 </div>
            </header>
         </div>
