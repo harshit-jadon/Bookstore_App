82f54a60103303e04a6904e99724e6f57d2a820b
harshit.jadon <harshit.jadon@MD-CJDKE06555KH.magicsw.com>
2021-08-18 23:34:55 +0530

    {Harshit} REFACTOR Login Page

--- src/components/loginPage/LoginPage.jsx
+++ src/components/loginPage/LoginPage.jsx
@@ -6,33 +6,33 @@ import bookbg from 'C:/Users/harshit.jadon.MD-CJDKE06555KH/Desktop/Bookstore App
 
 export default function LoginPage() {
     return (
-      <div className='body-div'>
-        <div className='main-div'>
+      <div className='body-lp-div'>
+        <div className='main-lp-div'>
           <div className='img-bor-div'>
-            <img src={bookbg}/>
+            <img className='img-lp'src={bookbg}/>
           </div>
-         <div className='form-div'>
-           <form className='form-div-2'>
-             <div className='form-title'>
+         <div className='form-lp-div'>
+           <form className='form-lp-div-2'>
+             <div className='form-lp-title'>
                 <span className='title'>Login to Bookcart</span>
              </div>
-             <div className='details-div'>
+             <div className='details-lp-div'>
                 <label>Username</label>
-                <input type="text" placeholder='Email Adrress / PhoneNumber'/> 
+                <input className='input-lp' type="text" placeholder='Email Adrress / PhoneNumber'/> 
               </div>
-              <div className='details-div'>
+              <div className='details-lp-div'>
                 <label>Password</label>
-                <input className='password' type='password' placeholder='Password'/>
+                <input className='password-lp' type='password' placeholder='Password'/>
               </div>
-              <div className='details-div2'>
+              <div className='details-lp-div2'>
                 <input type='radio'/><span>Remember me</span>
               </div>
-              <div className='details-div2'><a href=''><span>Forgot Password</span></a></div>
-              <div className='button-div'>
-                <button className='login-button'>Login</button>
-                <span className='or'> or </span>
-                <button className='login-button facebook'>Login with Facebook</button>
-                <button className='login-button google'>Login with Google Account</button>
+              <div className='details-lp-div2'><a href=''><span>Forgot Password</span></a></div>
+              <div className='button-lp-div'>
+                <button className='login-lp-button'>Login</button>
+                <span className='or-lp'> or </span>
+                <button className='login-lp-button facebook-lp'>Login with Facebook</button>
+                <button className='login-lp-button google-lp'>Login with Google Account</button>
               </div>
            </form>
          </div>
