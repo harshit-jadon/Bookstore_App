80076bbcb094da4c57a68dd7523de339d34ea89f
praveenrajbuggala <praveen8978470998rock@gmail.com>
2021-08-25 16:17:59 +0530

    {Praveen} added user info in checkout

--- src/components/orderPlaced/OrderPlaced.jsx
+++ src/components/orderPlaced/OrderPlaced.jsx
@@ -2,13 +2,36 @@ import React from 'react';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import LastPage from '../lastPage/LastPage'
-
+import BookStoreService from '../../service/BookStoreService';
 class OrderPlaced extends React.Component{
-    
+    constructor() {
+        super();
+        this.state={
+            userData:''
+        }
+        this.bookStoreService = new BookStoreService();
+    }
+    componentDidMount() {
+        this.getUser()
+    }
+
+    getUser = () => {
+        this.bookStoreService.getUser()
+            .then(responseDTO => {
+
+                let responseData = responseDTO;
+                console.log("Data received after GET Call :\n" + responseData.data.name);
+                this.setState({ userData: responseData.data });
+            }).then(()=>{
+            return this.state.userData
+        }).catch(errror => {
+            console.log("Error while fetching user\nError : " + JSON.stringify(errror));
+        })
+    }
     render(){
         return(
             <>
-            <Header />
+            <Header username={this.state.userData.name}/>
             <LastPage />
             <Footer />
             </>
